206497b8447944b03433c5e007879a7d57c39db8
Natalie Gonçalves <nataliegoncalvesdasilva02@gmail.com>
2021-11-28 14:58:15 -0300

    resolução do exercício senquencialimites

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace Divisao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.Write("Início: ");
+            int inicio = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Fim: ");
+            int fim = Convert.ToInt32(Console.ReadLine());
+
+            if (fim >= inicio)
+            {
+                int n = inicio;
+
+                while (n <= fim)
+                {
+                    Console.Write($"{n} ");
+                    n += 1;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não é possível terminar antes de iniciar.");
+            }
         }
     }
 }
